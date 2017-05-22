3332af8f6616bcd4f06b8b53951cee98f89219a0
Tomasz Gorochowik <tgorochowik@antmicro.com>
2017-05-22 12:46:57 +0200

    Improve sub-repositories handling
    All the sub-repositories are now a submodule. This commit removes the
    need to clone the container and the buildscripts repositories
    individually.

--- build.sh
+++ build.sh
@@ -1,28 +1,14 @@
 #!/usr/bin/env bash
 
-BUILDSCRIPTS_REPOSITORY=https://github.com/enclustra-bsp/enclustra-buildscripts.git
 BUILDSCRIPTS_RELEASE=v1.5
 BUILDSCRIPTS_DIR=buildscripts
 
-if [[ ! -e $BUILDSCRIPTS_DIR ]]; then
-  # Will attempt to clone buildscripts repository...
-  git clone $BUILDSCRIPTS_REPOSITORY $BUILDSCRIPTS_DIR -b $BUILDSCRIPTS_RELEASE &> /dev/null
-  if [[ ! $? -eq 0 ]]; then
-    echo "Could not clone buildscripts repository."
-    exit 1
-  fi
-else
-  pushd $BUILDSCRIPTS_DIR &> /dev/null
-  # Directory exists, checking if it is a valid git repository
-  if [[ ! $(git remote -v) == *"$BUILDSCRIPTS_REPOSITORY"* ]]; then
-    echo "There is something in the way. Please (re)move the" \
-         "\"$BUILDSCRIPTS_DIR\" directory to continue."
-    exit 1
-  fi
-  popd &> /dev/null
-fi
+# Will attempt to clone buildscripts repository...
+git submodule init
+git submodule update $BUILDSCRIPTS_DIR
 
 pushd $BUILDSCRIPTS_DIR &> /dev/null
+git checkout $BUILDSCRIPTS_RELEASE &> /dev/null
 git pull &> /dev/null
 popd &> /dev/null
 
