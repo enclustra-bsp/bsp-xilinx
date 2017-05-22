a6ded181855d738fbc3e1a4beae91205b53463ca
Tomasz Gorochowik <tgorochowik@antmicro.com>
2017-05-22 14:00:43 +0200

    Hide submodules modifications from users

--- build.sh
+++ build.sh
@@ -4,8 +4,14 @@ BUILDSCRIPTS_RELEASE=v1.5
 BUILDSCRIPTS_DIR=buildscripts
 
 # Will attempt to clone buildscripts repository...
-git submodule init
-git submodule update $BUILDSCRIPTS_DIR
+if ! [[ -e "$BUILDSCRIPTS_DIR/.git" ]]; then
+  git submodule init
+  git submodule update $BUILDSCRIPTS_DIR
+  git config submodule.buildscripts.ignore all
+  for s in $(ls sources/); do
+    git config submodule.sources/$s.ignore all
+  done
+fi
 
 pushd $BUILDSCRIPTS_DIR &> /dev/null
 git checkout $BUILDSCRIPTS_RELEASE &> /dev/null
