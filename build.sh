67dd702b056a4192e2f21eb36d1bdff34026a687
tholzsche <till.holzscheiter@enclustra.com>
2021-09-23 15:15:46 +0200

    build.sh: Get latest buildscripts.

--- build.sh
+++ build.sh
@@ -9,16 +9,17 @@ fi
 EBE_DEFAULT_RELEASE=update
 BUILDSCRIPTS_DIR=buildscripts
 
+[[ $EBE_RELEASE ]] || EBE_RELEASE=$EBE_DEFAULT_RELEASE
+
 # Will attempt to clone buildscripts repository...
 if ! [[ -e "$BUILDSCRIPTS_DIR/.git" ]]; then
   git submodule init
   git submodule update $BUILDSCRIPTS_DIR
-  git config submodule.buildscripts.ignore all
+  git config submodule.$BUILDSCRIPTS_DIR.ignore all
+  cd $BUILDSCRIPTS_DIR && git checkout $EBE_RELEASE && cd ..
   for s in $(ls sources/); do
     git config submodule.sources/$s.ignore all
   done
 fi
 
-[[ $EBE_RELEASE ]] || EBE_RELEASE=$EBE_DEFAULT_RELEASE
-
-./buildscripts/build.py --release $EBE_RELEASE $@
+./$BUILDSCRIPTS_DIR/build.py --release $EBE_RELEASE $@
